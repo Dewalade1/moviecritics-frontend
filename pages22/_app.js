c2e537f194832f71cc731c300f02ee8512f1aa10
Dewa <adewalade@gmail.com>
2021-03-01 19:45:16 +0000

    chore: folder structure cleanup
    delete unused folders and files

--- pages22/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import { init } from "../utils/sentry";
-
-init();
-
-export default function MyApp({ Component, pageProps, err }) {
-  // Workaround for https://github.com/vercel/next.js/issues/8592
-  return <Component {...pageProps} err={err} />;
-}
