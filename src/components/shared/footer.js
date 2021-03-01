bc0155489c0337b037645249f938218fac5be636
Dewa <adewalade@gmail.com>
2021-03-01 15:32:10 +0000

    feat: add sign up form to footer button

--- src/components/shared/footer.js
+++ src/components/shared/footer.js
@@ -1,20 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Link from 'next/Link';
 
-import { Form, FormControl } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
 import { Container, Row, Col } from "react-bootstrap";
 
 import Button from "@material-ui/core/Button";
+import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+import { withStyles } from '@material-ui/core/styles';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 
+import CloseIcon from '@material-ui/icons/Close';
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import RedditIcon from "@material-ui/icons/Reddit";
 
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
 
 const Footer = () => {
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <footer style={{ background: "#4f4e4e", color: "#ffffff" }} className="font-small pt-4 mt-4">
       <div className="pr-4 pl-4 pt-4 mt-4 pb-4 mb-4">
@@ -22,22 +82,22 @@ const Footer = () => {
           <Row>
             <Col md="2" className="text-left mt-4 pt-2">
               <ul>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Privacy Policy</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Terms</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Help</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Contact Us</a>
                   </Link>
@@ -49,22 +109,22 @@ const Footer = () => {
                 Quick Links
               </h6>
               <ul>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Home</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>About Us</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Actors</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Genres</a>
                   </Link>
@@ -76,17 +136,17 @@ const Footer = () => {
                 Our Apps
               </h6>
               <ul>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Android App</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Ios App</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Desktop Apps</a>
                   </Link>
@@ -99,17 +159,17 @@ const Footer = () => {
                 Tops
               </h6>
               <ul>
-                <li className="list-unstyled ">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Top Movies</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Top Celebs</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Top Genres</a>
                   </Link>
@@ -122,27 +182,27 @@ const Footer = () => {
                 Movies
               </h6>
               <ul>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Yoruba</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Igbo</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Hausa</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Christian</a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>Islamic</a>
                   </Link>
@@ -155,35 +215,35 @@ const Footer = () => {
                 Follow us
               </h6>
               <ul>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>
                       <FacebookIcon /> Facebook
                     </a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>
                       <TwitterIcon /> Twitter
                     </a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>
                       <InstagramIcon /> Instagram
                     </a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>
                       <YouTubeIcon /> YouTube
                     </a>
                   </Link>
                 </li>
-                <li className="list-unstyled">
+                <li className="list-unstyled text-decoration-none">
                   <Link href="#!">
                     <a style={{ color: "#ffffff" }}>
                       <RedditIcon /> Reddit
@@ -205,15 +265,45 @@ const Footer = () => {
                 </div>
               </Col>
               <Col className="text-left">
-                <Button type="submit" variant="contained" style={{ background: "#FF8C00" }} color="primary" component="span" className="mr-sm-2">
-                  Join now
-                </Button>
+                <div>
+                  <Button type="submit" variant="contained" style={{ background: "#FF8C00" }} color="primary" component="span" className="mr-sm-2" onClick={handleClickOpen}>
+                    Join now
+                  </Button>
+                  <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="customized-dialog-title" onClose={handleClose} inline>
+                      Sign Up
+                    </DialogTitle>
+                    <DialogContent dividers>
+                        <DialogContentText>
+                          To subscribe to this website, please enter your email address here. We will send updates
+                          occasionally.
+                        </DialogContentText>
+                        <TextField
+                          autoFocus
+                          variant="outlined"
+                          margin="dense"
+                          id="name"
+                          label="Email Address"
+                          type="email"
+                          fullWidth
+                        />
+                      </DialogContent>
+                      <DialogActions>
+                      <Button onClick={handleClose} color="primary">
+                        Cancel
+                      </Button>
+                      <Button onClick={handleClose} color="primary">
+                        Subscribe
+                      </Button>
+                  </DialogActions>
+                  </Dialog>
+                </div>
               </Col>
               <Col className=" pr-4 mr-4" style={{ color: "#A9A9A9" }}>
                 <h6>
                   &copy; {new Date().getFullYear()}{" "}
                   <Link href="#">
-                    <a style={{ color: "#ffffff" }} className=" pr-4 mr-4">
+                    <a style={{ color: "#ffffff" }} className="text-decoration-none pr-4 mr-4">
                       {" "}
                       Ellopod Consulting{" "}
                     </a>
