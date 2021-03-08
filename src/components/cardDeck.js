cd43357662b7f2d01cc97b1fcc41c885d24657e8
Dewa <adewalade@gmail.com>
2021-03-08 17:18:32 +0000

    feat: add more detail to movie cards on celeb page
    - Cards now show ratings
    - Cards now show Genre and year

--- src/components/cardDeck.js
+++ src/components/cardDeck.js
@@ -2,18 +2,19 @@ import {useState} from 'react';
 import Link from "next/link";
 import {CardDeck, Card } from 'react-bootstrap';
 
+import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 
-import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
+import Rating from "@material-ui/lab/Rating";
+import { positions } from "@material-ui/system";
+
+import StarBorderIcon from "@material-ui/icons/StarBorder";
 import AddBoxOutlinedIcon from "@material-ui/icons/AddBoxOutlined";
+import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
 
 export default function MyCardDeck ( {cardData} ) {
 
     const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
 
     return (
       <CardDeck>
@@ -21,30 +22,34 @@ export default function MyCardDeck ( {cardData} ) {
           <Card key={index}>
             <Link href="#!">
               <a>
-                <Card.Img variant="top" src={tile.img ? tile.img : "/images/movies/defaults/default-poster-1.jpg"} alt={cardData.title} />
+                <Card.Img variant="top" src={tile.img ? tile.img : "/images/movies/defaults/default-poster-1.jpg"} alt={tile.title} height={260}/>
               </a>
             </Link>
-            <Card.Body className="text-center">
-              <Card.Title>
-                <h6>{tile.title}</h6>
-              </Card.Title>
-              <Card.Text>
-                <p>{tile.genre}</p>
-                <p>{tile.year}</p>
-                <p>{tile.charPlayed}</p>
-              </Card.Text>
+            <Card.Body className="text-center pb-1">
+              <div className="align-text-top">
+                <Card.Title>
+                  <Link href="#!">
+                    <a style={{ color: "#fd7e14" }}>
+                      <h6>{tile.title}</h6>
+                    </a>
+                  </Link>
+                </Card.Title>
+                <Card.Text>
+                  <p>{tile.genre}</p>
+                  <p>{tile.year}</p>
+                  <Box component="fieldset" mb={3} borderColor="transparent">
+                    <Rating name="read-only" value={tile.rating} precision={0.1} readOnly emptyIcon={<StarBorderIcon fontSize="inherit" />} />
+                  </Box>
+                </Card.Text>
+              </div>
             </Card.Body>
-            <Card.Footer className="text-center">
-              <Button className="mt-sm-2" style={{ background: "#fd7e14", color: "#ffffff" }}>
+            <Card.Footer className="text-center pl-2 pr-2">
+              <Button className="mt-sm-2" fullWidth style={{ background: "#fd7e14", color: "#ffffff" }}>
                 <AddBoxOutlinedIcon className="mr-sm-2" /> Watchlist
               </Button>
-              <Link href="#!">
-                <a target="_blank">
-                  <Button className="mt-sm-2 mb-sm-2">
-                    <PlayArrowRoundedIcon className="mr-sm-2" /> Trailer
-                  </Button>
-                </a>
-              </Link>
+              <Button href={tile.url} className="mt-sm-2 mb-sm-2" style={{ color: "#4f4e4e" }}>
+                <PlayArrowRoundedIcon className="mr-sm-2" /> Trailer
+              </Button>
             </Card.Footer>
           </Card>
         ))}
--- src/pages/celebs/celebs.js
+++ src/pages/celebs/celebs.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import Layout from '../../layouts/layout';
 import MyCarousel from '../../../src/components/carousel';
 import ImageGridList from '../../components/imageGridList';
-import MyCardDeck from "../../components/CardDeck";
+import MyCardDeck from "../../components/cardDeck";
 import DialogBtn from '../../components/buttons/dialogButton';
 
 import { Container, Row, Col } from "react-bootstrap";
@@ -37,6 +37,9 @@ export default function Celeb() {
     const tileData = {
       name: "Ramsey Nouah",
       positions: ["Actor", "Director", "Producer"],
+      likes: "30k",
+      dislikes: "2k",
+      favs: "10k",
       bio: ['In 1996, he made his major screen movie debut in the well received Silent Night after performing in a few Nigeria home videos (now widely known as Nollywood) projects which never got released due to lack of completion funds and related issues. Succeeded by numerous Nigerian home video projects where he mainly played romantic roles, from the 1990\'s through the early 2000\'s; Nouah was quickly labelled "Lover Boy". In 2004, he starred in Dangerous Twins (2004) and his stellar performance as a good and bad twin, Taiye and Kehinde, projected him as a multi layered actor capable of much more his "Lover Boy" image had suggested. By then, his fame had transcend the shores of Nigeria and was exceeding Africa.', "His face was on numerous movie posters and his performance progressively got stronger and attracted acclaim as he sold millions of Nigerian home video (which was straight to DVD) projects within lovers of African entertainment. He is an African household name that won't be forgotten for his immense contribution to the African entertainment industry at large. In 2009, he portrayed a socially awkward fresh graduate in The Figurine (2009). He is often regarded as one of Africa's greatest actor of all time.", "With an amazing talent and acting range, he has managed to remain new and fresh even after more than two decades in the industry. He has won numerous awards for his acting and contribution to the African entertainment industry in Nigeria and around the world. In 2010, he won Best Actor at the African Movie Academy Awards for his performance in The Figurine (2009).He is a one of the few active pioneers and ambassador of the laudable Nigerian movie industry also known as Nollywood. His burning desire to add to the creative growth of Nollywood and African cinema led to his founding a movie production company called Ramseyfilms."],
       img: {
         celeb: [
@@ -94,45 +97,51 @@ export default function Celeb() {
         movies: [
           {
             img: "/images/celebs/Ramsey-Noah/media/merry-men-2.jpg",
-            url: "",
+            url: "https://www.youtube.com/watch?v=thx-M3hTGLs",
             title: "Merry Men 2",
             year: "2019",
-            genre: "",
+            genre: "Action | Comedy",
+            rating: 4.0,
           },
           {
             img: "/images/celebs/Ramsey-Noah/media/living-in-bondage-poster.jpg",
-            url: "",
-            title: "Living in Bondage",
+            url: "https://www.youtube.com/watch?v=IhvJG_c8Kgw",
+            title: "Living in Bondage II",
             year: "2019",
-            genre: "",
+            genre: "Drama",
+            rating: 3.5,
           },
           {
             img: "/images/celebs/Ramsey-Noah/media/soul-tie.jpg",
-            url: "",
+            url: "https://www.youtube.com/watch?v=7wE5o_RFPDE",
             title: "Soul Tie",
             year: "2017",
-            genre: "",
+            genre: "Drama",
+            rating: 4.5,
           },
           {
             img: "/images/celebs/Ramsey-Noah/media/my-wife-and-i.jpg",
-            url: "",
+            url: "https://www.youtube.com/watch?v=IKP2IXIeJPE",
             title: "My Wife and I",
             year: "2017",
-            genre: "",
+            genre: "Comedy | Family",
+            rating: 2.0,
           },
           {
             img: "/images/celebs/Ramsey-Noah/media/body-language.jpg",
-            url: "",
+            url: "https://www.youtube.com/watch?v=LfpyNi1_dqE",
             title: "Body Language",
             year: "2017",
-            genre: "",
+            genre: "Drama | Thriller",
+            rating: 3.2,
           },
           {
             img: "/images/celebs/Ramsey-Noah/media/gbomo-gbomo-express.jpg",
-            url: "",
+            url: "https://www.youtube.com/watch?v=Vi3N9B97m38",
             title: "Gbomo Gbomo Express",
             year: "2015",
-            genre: "",
+            genre: "Comedy | Crime",
+            rating: 5.0,
           },
         ],
       },
@@ -140,7 +149,7 @@ export default function Celeb() {
         btnText: "Learn More",
         fullWidth: true,
         maxWidth: "md",
-        dialogFooterBtn: true,
+        dialogFooterBtn: false,
       },
     };
     
@@ -159,16 +168,19 @@ export default function Celeb() {
                   }
                   action={
                     <CardActions disableSpacing>
+                      {tileData.likes}
                       <Tooltip title="Like" placement="top">
                         <IconButton aria-label="like">
                           <ThumbUpRoundedIcon />
                         </IconButton>
                       </Tooltip>
+                      {tileData.dislikes}
                       <Tooltip title="Dislike" placement="top">
                         <IconButton aria-label="dislike">
                           <ThumbDownRoundedIcon />
                         </IconButton>
                       </Tooltip>
+                      {tileData.favs}
                       <Tooltip title="Add to Favourites" placement="top">
                         <IconButton aria-label="add to favorites">
                           <FavoriteIcon />
