d48e281ab0c87dd3d5e891a782004b8fe228b121
Dewa <adewalade@gmail.com>
2021-03-22 02:37:41 +0000

    fix: fix home page display bug
    There was an error in the carousel that made the homepage not display
    but it has been fixed.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,30 +3,61 @@ import Link from 'next/link';
 
 import {Carousel} from 'react-bootstrap';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 60%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}));
+
 export default function MyCarousel({tileData}) {
   const [index, setIndex] = useState(0);
+  const classes = useStyles();
 
-  const handleSelect = (selectedIndex, e) => {
+  const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
+
+  
 
   return (
     <>
     <Carousel activeIndex={index} onSelect={handleSelect}>
       {tileData.map((tile, index) => { 
+
+        let link = '';
+
+          if (tile.fields.trailerUrl) {
+            const link = tile.fields.trailerUrl
+          } else if (tile.fields.description) {
+            const link = tile.fields.description
+          } else {
+            const link = tile.fields.file.url
+          }
+
+
           return(          
-            <Carousel.Item key={index}>
-                <Link href={tile.fields.description ? tile.fields.description : tile.fields.file.url}>
+            <Carousel.Item key={tile.sys.id}>
+                <Link href={tile.fields.trailerUrl ? tile.fields.trailerUrl : link}>
                   <a target="_blank">
                     <img
-                        className="d-block w-100"
-                        src={tile.img ? tile.img : tile.fields.file.url}
-                        alt={tile.title ? tile.title : tile.fields.title}
+                        className={`d-block w-100`}
+                        src={tile.fields.media1 ? tile.fields.media1.fields.file.url : tile.fields.file.url}
+                        alt={tile.fields.title}
                         width={700}
                         height={600}
                     />
                   </a>
                 </Link>
+                <Carousel.Caption className={{root: classes.titleBar, title: classes.title,}}>
+                    <h3>{tile.fields.title}</h3>
+                    <p>{tile.fields.description1}</p>
+                </Carousel.Caption>
             </Carousel.Item>  
         );
     })}
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,106 +12,26 @@ import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CardActionArea from '@material-ui/core/CardActionArea';
 
-export default function Home({response}) {
-
-  const tileData = [{
-        img: "/images/movies/posters/thy-will-be-done-1.jpg",
-        title: 'First slide label', 
-        desc: "Nulla vitae elit libero, a pharetra augue mollis interdum.", 
-        url: "https://www.youtube.com/watch?v=QqLsF9miflw",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/citation.jpg",
-        title: 'Third slide label', 
-        desc: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
-        url: "https://www.youtube.com/watch?v=1eMAYynMc1w", 
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/In-every-way.jpg",
-        title: 'Fourth slide label', 
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-        url:"https://www.youtube.com/watch?v=sz2dmMyeX04",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/your-excellency.jpg",
-        title: 'Fifth slide label', 
-        desc: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
-        url: "https://www.youtube.com/watch?v=pAMUldixCu4", 
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/espionage.jpg",
-        title: 'Sixth slide label', 
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-        url: "https://www.youtube.com/watch?v=_OfNjzLLZUo",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/put-a-ring-on-it.webp",
-        title: 'Seventh slide label', 
-        desc: "Nulla vitae elit libero, a pharetra augue mollis interdum.", 
-        url: "https://www.youtube.com/watch?v=FGwXDed3VwA",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/kambili.jpg",
-        title: 'Second slide label', 
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-        url: "https://www.youtube.com/watch?v=jqRpjk0m4_c",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/the-wedding-party.webp",
-        title: 'Eighth slide label', 
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-        url: "https://www.youtube.com/watch?v=zbnXd-zCD6I",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/so-in-love.jpg",
-        title: 'Ninth slide label', 
-        desc: "Nulla vitae elit libero, a pharetra augue mollis interdum.", 
-        url: "https://www.youtube.com/watch?v=kkbCleVJq7U",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/angels-heart.jpg",
-        title: "Angel's Heart", 
-        desc: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.o", 
-        url: "https://www.youtube.com/watch?v=z-WIAD4LgnI",
-        author: "Dewa Ade-Onojobi"
-    },{
-        img: "/images/movies/posters/bonds-of-love.jpg",
-        title: "Bonds of Love", 
-        desc: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.o", 
-        url: "https://www.youtube.com/watch?v=oIoWY9sTeEQ",
-        author: "Dewa Ade-Onojobi"
-    }]
+export default function Home({celebs, carouselData}) {
 
   return (
     <Layout>
-      <Card className="text-center">
-        {/* put card in carousel for titles */}
-        <MyCarousel tileData={tileData} />
-        <CardActionArea>
-            <CardContent>
-                {/* <Typography gutterBottom variant="h4" component="h2">
-                    <h3>{tile.title}</h3>
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    <p>{tile.desc}</p>
-                  </Typography> */}
-            </CardContent>
-        </CardActionArea>
+      <Card className="text-center"  variant="outlined" id="main-carousel">
+        <MyCarousel tileData={carouselData} />
       </Card>
 
-      <Row className="mt-4">
+      <Row className="mt-4" id="celebrities">
         <Col>
             <Card className="mb-4 pl-2" variant="outlined">
               <CardContent>
-                <CardHeader title="Celebrities" />
+                <CardHeader title="Celebrities" style={{color:"#FF8C00"}}/>
                   <CardDeck>
-                    {response.map((data) => (
+                    {celebs.map((data) => (
                         <CardList tileData={data} key={data.sys.id} />
                     ))}
                   </CardDeck>
               </CardContent>
             </Card>
-
         </Col>
       </Row>
     </Layout>
@@ -119,15 +39,20 @@ export default function Home({response}) {
 }
 
 export async function getServerSideProps() {
-  const res = await fetchEntries('celebs');
-
-  const response = await res.map((celeb) => {
+  const rawCelebs = await fetchEntries('celebs');
+  const celebs = await rawCelebs.map((celeb) => {
     return celeb;
+  });
+
+  const rawCarouselData = await fetchEntries('carousel');
+  const carouselData = await rawCarouselData.map((data) => {
+      return data;
   });
 
   return {
     props: {
-      response,
+      celebs,
+      carouselData,
     },
   };
 }
