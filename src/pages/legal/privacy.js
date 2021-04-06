60507567027c56e8a8eb92f42a43e2960d014be5
Dewa <adewalade@gmail.com>
2021-04-06 16:40:50 +0100

    feat: add files for legal content

---
+++ src/pages/legal/privacy.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Layout from "../../layouts/layout";
+
+export default function Privacy() {
+  return <Layout></Layout>;
+}
---
+++ src/pages/legal/terms.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Layout from "../../layouts/layout";
+
+export default function Terms() {
+  return <Layout></Layout>;
+}
